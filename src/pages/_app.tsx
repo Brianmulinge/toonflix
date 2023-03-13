5c33e9dea53aa338c0f8678cfc95b3ba2bc2775e
Brianjaden <brianjaden45@gmail.com>
2023-03-13 04:57:55 -0500

    updated UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import "../styles/globals.css";
+import { Inter } from "next/font/google";
 
-import "~/styles/globals.css";
+const inter = Inter({ subsets: ["latin"] });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
