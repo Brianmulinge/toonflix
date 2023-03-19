8d8ab2d3b73cba360003344f746b4602a238d06d
Brianjaden <brianjaden45@gmail.com>
2023-03-19 04:18:14 -0500

    tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require("tailwind-scrollbar-hide");
+
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [plugin],
 };
 
 module.exports = config;
