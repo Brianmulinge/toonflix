f8aaada8c4a05953b8d4715ca867971b1e094b2f
brianmulinge <brianjaden45@gmail.com>
2025-03-19 18:59:27 -0500

    made changes to the trailer and movies api routes

--- app/api/movies/[id]/trailer/route.ts
+++ app/api/movies/[id]/trailer/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const id = params.id;
--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import {  NextResponse } from 'next/server';
 
 const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
